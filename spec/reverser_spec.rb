88e5828e2534d3a45b8feeddf9097e70ea61e10d
Dragos Tirlungeanu <Dragos@Mackie-Pro.local>
2018-02-16 16:35:55 +0000

    this is it.. maybe?

--- spec/reverser_spec.rb
+++ spec/reverser_spec.rb
@@ -2,7 +2,7 @@ require "reverser"
 
 describe Reverser do
   it "should reverse a string" do
-    expect(Reverser.reverse "hello").to eq("olleh")
+    expect(Reverser.reverse "hello").to eq("oleh")
   end
 end
 
